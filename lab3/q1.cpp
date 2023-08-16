45e6397b0f7f81ab5b41478dc0b96d0e07eafe10
Naman Kale <namankale2406@gmail.com>
2023-08-16 19:00:05 +0530

    q1 solved

---
+++ lab3/q1.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Queue{
+    private:
+        stack<int> s1;
+        stack<int> s2;
+        int size = 0;
+
+    public:
+        
+
+        
+
+        void enqueue(int a){
+            for (int i = 0;i<size;i++){
+                int k = s1.top();
+                s1.pop();
+                s2.push(k);
+            }
+            s1.push(a);
+            for (int i = 0;i<size;i++){
+                int k = s2.top();
+                s2.pop();
+                s1.push(k);
+            }
+            size++;
+        }
+
+        int dequeue(){
+            int k = s1.top();
+            s1.pop();
+            size--;
+            return k;
+        }
+
+        bool empty(){
+            if (size ==0) return true;
+            else return false;
+        }
+
+};
+
+
+int main(){
+    Queue* q = new Queue();
+    q->enqueue(10);
+    q->enqueue(20);
+    cout<<q->dequeue()<<endl;
+
+}
+
